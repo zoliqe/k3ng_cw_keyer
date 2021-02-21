2ff84278f01d9858d4f9dc24108423d953edec05
Michal Karas <michal.karas@posam.sk>
2021-02-21 08:21:26 +0100

    merge_upstream.sh

--- merge_upstream.sh
+++ merge_upstream.sh
@@ -1,6 +1,9 @@
-#git remote -v
-#git remote add upstream https://github.com/k3ng/k3ng_cw_keyer.git
 git remote -v
+echo "add upstream repo to remotes"
+git remote add upstream https://github.com/k3ng/k3ng_cw_keyer.git
+git remote -v
+echo "fetch upstream"
 git fetch upstream
 git checkout master
+echo "merge upstream/master commits"
 git merge upstream/master
